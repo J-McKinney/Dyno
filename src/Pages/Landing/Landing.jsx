1e2be386ab18d148970dc886c82d81ec69b6a985
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-03 19:01:24 -0400

    yay

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import LandingStyle from "./Landing.module.css";
 
 class Landing extends Component {
@@ -6,7 +9,13 @@ class Landing extends Component {
     return (
       <>
         <div className={LandingStyle.wrapper}>
-          <div className={LandingStyle.header}>Header</div>
+          <Container className={LandingStyle.container}>
+            Container
+            <Row className={LandingStyle.row}>
+              Row
+              <Col className={LandingStyle.col}>Col</Col>
+            </Row>
+          </Container>
         </div>
       </>
     );
