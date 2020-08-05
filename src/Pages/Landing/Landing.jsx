2c340b09e8c2b7e4f7bf04c88ecefbcfcbe4545c
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-05 13:15:15 -0400

    yay

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -3,6 +3,7 @@ import NavBar from "../../Components/NavBar/NavBar";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import MyPic from "../../images/MeDuh.JPG";
 import LandingStyle from "./Landing.module.css";
 
 class Landing extends Component {
@@ -12,8 +13,20 @@ class Landing extends Component {
         <NavBar />
         <Container className={LandingStyle.firstContainer}>
           <Row className={LandingStyle.firstRow}>
-            <Col className={LandingStyle.firstCol}></Col>
-            <Col className={LandingStyle.secondCol}></Col>
+            <Col className={LandingStyle.firstCol}>
+              <p>
+                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum Lorem
+                Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+              </p>
+            </Col>
+            <Col className={LandingStyle.secondCol}>
+              <img
+                className={LandingStyle.myPic}
+                src={MyPic}
+                alt="Jesse McKinney"
+              />
+            </Col>
           </Row>
         </Container>
       </div>
